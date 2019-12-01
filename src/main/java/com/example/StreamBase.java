e408fa2f46f557274af74344e19d16c9586587e3
lilei <tothis@qq.com>
2019-12-01 16:14:37 +0800

    initial

--- src/main/java/com/example/StreamBase.java
+++ src/main/java/com/example/StreamBase.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.apache.commons.collections4.ListUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -258,9 +260,79 @@ public class StreamBase {
                 , (var1, var2) -> var1 + var2
                 )
         );
+
+        int SPLIT_NUMBER = 3;
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+        // java手写方式
+        splitList1(list, SPLIT_NUMBER).forEach(System.out::println);
+        // stream方式
+        splitList2(list, SPLIT_NUMBER).forEach(System.out::println);
+        // 使用apache common collection对List进行分割
+        ListUtils.partition(list, SPLIT_NUMBER).forEach(System.out::println);
+        // 使用google guava对List进行分割 把iterable按指定大小分割 得到的子集都不能进行修改操作
+        Lists.partition(list, SPLIT_NUMBER).forEach(System.out::println);
     }
 
     private static void out(String name, long count, long start) {
         System.out.printf("%-4s count:%d time:%dms\n", name, count, System.currentTimeMillis() - start);
+    }
+
+    /**
+     * java手动实现
+     *
+     * @return
+     */
+    private static <T> List<List<T>> splitList1(List<T> source, int split) {
+        List<List<T>> result = new ArrayList<>();
+        //(先计算出余数)
+        int remainder = source.size() % split;
+        //然后是商
+        int number = source.size() / split;
+        //偏移量
+        int offset = 0;
+        for (int i = 0; i < split; i++) {
+            List<T> value;
+            if (remainder > 0) {
+                value = source.subList(i * number + offset, (i + 1) * number + offset + 1);
+                remainder--;
+                offset++;
+            } else {
+                value = source.subList(i * number + offset, (i + 1) * number + offset);
+            }
+            result.add(value);
+        }
+        return result;
+    }
+
+    /**
+     * stream实现
+     *
+     * @return
+     */
+    private static <T> List<List<T>> splitList2(List<T> source, int split) {
+        int limit = (source.size() + split - 1) / split;
+        // 方法一 使用流遍历操作
+        List<List<T>> splitList = new ArrayList<>();
+        Stream.iterate(0, n -> n + 1).limit(limit).forEach(i -> {
+            splitList.add(source.stream()
+                    .skip(i * split)
+                    .limit(split)
+                    .collect(Collectors.toList())
+            );
+        });
+
+        // 方法二 获取分割后的集合
+//        splitList = Stream
+//                .iterate(0, n -> n + 1)
+//                .limit(limit)
+//                .parallel()
+//                .map(a -> source.stream()
+//                        .skip(a * split)
+//                        .limit(split)
+//                        .parallel()
+//                        .collect(Collectors.toList())
+//                ).collect(Collectors.toList());
+
+        return splitList;
     }
 }
