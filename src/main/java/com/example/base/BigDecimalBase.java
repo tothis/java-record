d4cfe1da51ac7385cb27a4bde9306dcb0e56e723
lilei <tothis@qq.com>
2020-02-23 15:33:37 +0800

    initial

--- src/main/java/com/example/base/BigDecimalBase.java
+++ src/main/java/com/example/base/BigDecimalBase.java
@@ -38,43 +38,49 @@ public class BigDecimalBase {
         /**
          * 参数一为除数 参数二为小数点后保留位数 参数三为舍入模式
          *
-         * 在运算除法或四舍五入时要用到到舍入模式
-         *
-         * ROUND_UP 对非舍弃部分最后一位数字加1
+         * ROUND_UP 为正数对非舍弃部分最后一位数字加1 为负数减1
          * ROUND_DOWN 直接舍弃
          * ROUND_CEILING 为正数结果同ROUND_UP 为负数结果同ROUND_DOWN
          * ROUND_FLOOR 为正数结果同ROUND_DOWN 为负数结果同ROUND_UP
-         * ROUND_HALF_UP 向 最接近 的数字舍入 如果与两个相邻数字的距离相等 则为向上舍入的舍入模式 如果舍弃部分>= 0.5 则舍入行为与ROUND_UP相同；否则舍入行为与ROUND_DOWN相同 这种模式也就是我们常说的我们的 四舍五入
-         * ROUND_HALF_DOWN 向 最接近 的数字舍入 如果与两个相邻数字的距离相等 则为向下舍入的舍入模式 如果舍弃部分> 0.5 则舍入行为与ROUND_UP相同；否则舍入行为与ROUND_DOWN相同 这种模式也就是我们常说的我们的 五舍六入
-         * ROUND_HALF_EVEN 向 最接近 的数字舍入 如果与两个相邻数字的距离相等 则相邻的偶数舍入 如果舍弃部分左边的数字奇数 则舍入行为与 ROUND_HALF_UP 相同；如果为偶数 则舍入行为与 ROUND_HALF_DOWN 相同 注意：在重复进行一系列计算时 此舍入模式可以将累加错误减到最小 此舍入模式也称为 银行家舍入法  主要在美国使用 四舍六入 五分两种情况 如果前一位为奇数 则入位 否则舍去
-         * ROUND_UNNECESSARY 不需要取整 如存在小数位 抛出异常ArithmeticException
+         * ROUND_HALF_UP 四舍五入
+         * ROUND_HALF_DOWN 与ROUND_HALF_UP区别在于0.5时会向下取整
+         * ROUND_HALF_EVEN 银行家舍入算法
+         * ROUND_UNNECESSARY 不进行舍入 如存在无限小数 抛出异常ArithmeticException
          */
-        roundDecimal(".11", BigDecimal.ROUND_UP); // 0.2
-        roundDecimal("-.11", BigDecimal.ROUND_UP); // -0.2
-        roundDecimal(".19", BigDecimal.ROUND_DOWN); // 0.1
-        roundDecimal("-.19", BigDecimal.ROUND_DOWN); // -0.1
+        roundDecimal(".01", BigDecimal.ROUND_UP); // 0.1
+        roundDecimal("-.01", BigDecimal.ROUND_UP); // -0.1
+        roundDecimal(".09", BigDecimal.ROUND_DOWN); // 0.0
+        roundDecimal("-.09", BigDecimal.ROUND_DOWN); // 0.0
+        System.out.println("-----");
 
-        roundDecimal(".11", BigDecimal.ROUND_CEILING); // 0.2
-        roundDecimal("-.11", BigDecimal.ROUND_CEILING); // -0.1
-        roundDecimal(".19", BigDecimal.ROUND_FLOOR); // 0.1
-        roundDecimal("-.19", BigDecimal.ROUND_FLOOR); // -0.2
+        roundDecimal(".01", BigDecimal.ROUND_CEILING); // 0.1
+        roundDecimal("-.01", BigDecimal.ROUND_CEILING); // 0.0
+        roundDecimal(".09", BigDecimal.ROUND_FLOOR); // 0.0
+        roundDecimal("-.09", BigDecimal.ROUND_FLOOR); // -0.1
+        System.out.println("-----");
 
         // 四舍五入
-        roundDecimal(".15", BigDecimal.ROUND_HALF_UP); // 0.2
-        roundDecimal("-.15", BigDecimal.ROUND_HALF_UP); // -0.2
+        roundDecimal(".05", BigDecimal.ROUND_HALF_UP); // 0.1
+        roundDecimal("-.05", BigDecimal.ROUND_HALF_UP); // -0.1
+        System.out.println("-----");
+
         // 和ROUND_HALF_UP区别在于0.5时会向下取整
-        roundDecimal(".15", BigDecimal.ROUND_HALF_DOWN); // 0.1
-        roundDecimal("-.15", BigDecimal.ROUND_HALF_DOWN); // -0.1
-        roundDecimal(".16", BigDecimal.ROUND_HALF_DOWN); // 0.2
-        roundDecimal("-.16", BigDecimal.ROUND_HALF_DOWN); // -0.2
+        roundDecimal(".05", BigDecimal.ROUND_HALF_DOWN); // 0.0
+        roundDecimal("-.05", BigDecimal.ROUND_HALF_DOWN); // 0.0
+        roundDecimal(".06", BigDecimal.ROUND_HALF_DOWN); // 0.1
+        roundDecimal("-.06", BigDecimal.ROUND_HALF_DOWN); // -0.1
+        System.out.println("-----");
 
         // 银行家舍入 https://gitee.com/tothis/javascript-record/blob/master/base/number.html
         // 四舍六入五留双 => 四舍六入五考虑 五后非零就进一 五后为零看奇偶 五前为偶应舍去 五前为奇要进一
-        roundDecimal(".11", BigDecimal.ROUND_HALF_EVEN);
-        roundDecimal(".11", BigDecimal.ROUND_HALF_EVEN);
+        roundDecimal(".05", BigDecimal.ROUND_HALF_EVEN); // 0.0
+        roundDecimal(".051", BigDecimal.ROUND_HALF_EVEN); // 0.1
+        roundDecimal(".15", BigDecimal.ROUND_HALF_EVEN); // 0.2
+        roundDecimal(".85", BigDecimal.ROUND_HALF_EVEN); // 0.8
+        System.out.println("-----");
 
         // 不会舍人 默认舍人类型
-        roundDecimal(".1", BigDecimal.ROUND_UNNECESSARY);
+        roundDecimal("0", BigDecimal.ROUND_UNNECESSARY); // 0.0
     }
 
     private static void roundDecimal(String value, int type) {
