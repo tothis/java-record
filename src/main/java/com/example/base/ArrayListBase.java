fa2cf5a86794c353cff4e1ad3f90494a87e1973e
lilei <tothis@qq.com>
2020-04-15 13:38:05 +0800

    initial

--- src/main/java/com/example/base/ArrayListBase.java
+++ src/main/java/com/example/base/ArrayListBase.java
@@ -1,8 +1,12 @@
 package com.example.base;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * @author 李磊
@@ -29,12 +33,12 @@ public class ArrayListBase {
      */
     public static void main(String[] args) {
         // list编辑时需要删除元素 可使用iterator
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) list.add(i);
-        Iterator<Integer> iterator = list.iterator();
+        List<Long> list = Stream.iterate(0L, i -> i + 1)
+                .limit(5).collect(toList());
+        Iterator<Long> iterator = list.iterator();
         while (iterator.hasNext()) {
-            Integer next = iterator.next();
-            if (next.equals(3)) {
+            Long next = iterator.next();
+            if (next.equals(3L)) {
                 iterator.remove();
                 // 此次循环依然可以访问 下次循环中list无此元素
                 System.out.println(next);
@@ -44,7 +48,54 @@ public class ArrayListBase {
         list.forEach(System.out::println);
 
         // 指定索引位置添加元素
-        list.add(3, 3);
+        list.add(3, 3L);
         System.out.println(list);
+
+        /**
+         * 调用Arrays.asList()创建的List add remove方法 抛出UnsupportedOperationException异常
+         * Arrays.asList() 返回为Arrays内部类ArrayList 并非java.util.ArrayList
+         * Arrays内部类ArrayList和java.util.ArrayList同为继承AbstractList
+         * remove add等方法 AbstractList中默认throw UnsupportedOperationException且不作任何操作
+         * 解决方法使用java.util.ArrayList构造器构造 `new ArrayList(Arrays.asList(...))`
+         */
+        List<String> list1 = new ArrayList(Arrays.asList("a,b,c,d,e".split(",")));
+
+        List<String> list2 = new ArrayList(Arrays.asList("a,b,c".split(",")));
+
+        // list1删除list2已有的数据
+        // list2.forEach(item1 -> list1.removeIf(item2 -> item2.equals(item1)));
+        // list1.forEach(System.out::println);
+
+        // 交集
+        List<String> intersection = list1.stream().filter(item -> list2.contains(item)).collect(toList());
+        System.out.println("---交集 intersection---");
+        intersection.parallelStream().forEach(System.out::println);
+
+        // 差集 (list1 - list2)
+        List<String> reduce1 = list1.stream().filter(item -> !list2.contains(item)).collect(toList());
+        System.out.println("---差集 reduce1 (list1 - list2)---");
+        reduce1.parallelStream().forEach(System.out::println);
+
+        // 差集 (list2 - list1)
+        List<String> reduce2 = list2.stream().filter(item -> !list1.contains(item)).collect(toList());
+        System.out.println("---差集 reduce2 (list2 - list1)---");
+        reduce2.parallelStream().forEach(System.out::println);
+
+        // 并集
+        List<String> listAll1 = list1.parallelStream().collect(toList());
+        List<String> listAll2 = list2.parallelStream().collect(toList());
+        listAll1.addAll(listAll2);
+        System.out.println("---并集 listAll1---");
+        listAll1.parallelStream().forEachOrdered(System.out::println);
+
+        // 去重并集
+        List<String> listAllDistinct = listAll1.stream().distinct().collect(toList());
+        System.out.println("---得到去重并集 listAllDistinct---");
+        listAllDistinct.parallelStream().forEachOrdered(System.out::println);
+
+        System.out.println("---原来的List1---");
+        list1.parallelStream().forEachOrdered(System.out::println);
+        System.out.println("---原来的List2---");
+        list2.parallelStream().forEachOrdered(System.out::println);
     }
 }
--- src/main/java/com/example/util/DateUtil.java
+++ src/main/java/com/example/util/DateUtil.java
@@ -1,9 +1,8 @@
 package com.example.util;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 /**
  * @author 李磊
@@ -16,33 +15,175 @@ public class DateUtil {
     public static final String FORMAT_DATE = "yyyy-MM-dd";
     public static final String FORMAT_TIME = "HH:mm:ss";
 
+    /**
+     * 当前日期和时间
+     *
+     * @return
+     */
     public static String dateTime(String format) {
         return LocalDateTime.now().format(DateTimeFormatter.ofPattern(format));
     }
 
-    public static String date(String format) {
-        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
-    }
-
-    public static String time(String format) {
-        return LocalTime.now().format(DateTimeFormatter.ofPattern(format));
-    }
-
+    /**
+     * 当前日期和时间
+     *
+     * @return
+     */
     public static String dateTime() {
         return dateTime(FORMAT);
     }
 
+    /**
+     * 当前日期
+     *
+     * @return
+     */
+    public static String date(String format) {
+        return LocalDate.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * 当前日期
+     *
+     * @return
+     */
     public static String date() {
         return date(FORMAT_DATE);
     }
 
+    /**
+     * 当前时间
+     *
+     * @return
+     */
+    public static String time(String format) {
+        return LocalTime.now().format(DateTimeFormatter.ofPattern(format));
+    }
+
+    /**
+     * 当前时间
+     *
+     * @return
+     */
     public static String time() {
         return time(FORMAT_TIME);
+    }
+
+    /**
+     * date转为ZonedDateTime
+     *
+     * @param date
+     * @return
+     */
+    public static ZonedDateTime dateToZonedDateTime(Date date) {
+        return date.toInstant().atZone(ZoneId.systemDefault());
+    }
+
+    /**
+     * 获取Date日期和时间
+     *
+     * @return
+     */
+    public static String dateTime(Date date) {
+        return dateToLocalDateTime(date)
+                .format(DateTimeFormatter.ofPattern(FORMAT));
+    }
+
+    /**
+     * 获取Date日期
+     *
+     * @return
+     */
+    public static String date(Date date) {
+        return dateToLocalDate(date)
+                .format(DateTimeFormatter.ofPattern(FORMAT_DATE));
+    }
+
+    /**
+     * 获取Date时间
+     *
+     * @return
+     */
+    public static String time(Date date) {
+        return dateToLocalTime(date)
+                .format(DateTimeFormatter.ofPattern(FORMAT_TIME));
+    }
+
+    /**
+     * Date转为LocalDate
+     *
+     * @param date
+     * @return
+     */
+    public static LocalDate dateToLocalDate(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant()
+                , ZoneId.systemDefault()).toLocalDate();
+        // return dateToZonedDateTime(date).toLocalDate();
+    }
+
+    /**
+     * Date转为LocalTime
+     *
+     * @param date
+     * @return
+     */
+    public static LocalTime dateToLocalTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant()
+                , ZoneId.systemDefault()).toLocalTime();
+        // return dateToZonedDateTime(date).toLocalTime();
+    }
+
+    /**
+     * Date转为LocalDateTime
+     *
+     * @param date
+     * @return
+     */
+    public static LocalDateTime dateToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+        // return dateToZonedDateTime(date).toLocalDateTime();
+    }
+
+    /**
+     * LocalDate转为Date
+     *
+     * @param localDate
+     * @return
+     */
+    public static Date localDateToDate(LocalDate localDate) {
+        return Date.from(localDate.atStartOfDay()
+                .atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * LocalTime转为Date
+     *
+     * @param localTime
+     * @return
+     */
+    public static Date localTimeToDate(LocalTime localTime) {
+        return Date.from(LocalDateTime.of(LocalDate.now(), localTime)
+                .atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    /**
+     * LocalDateTime转为Date
+     *
+     * @param localDateTime
+     * @return
+     */
+    public static Date localDateTimeToDate(LocalDateTime localDateTime) {
+        return Date.from(localDateTime.atZone(ZoneId.systemDefault())
+                .toInstant());
     }
 
     public static void main(String[] args) {
         System.out.println(dateTime());
         System.out.println(date());
         System.out.println(time());
+        System.out.println(LocalTime.now());
+        System.out.println(date(localDateToDate(LocalDate.now())));
+        System.out.println(time(localTimeToDate(LocalTime.now())));
+        System.out.println(dateTime(localDateTimeToDate(LocalDateTime.now())));
     }
 }
